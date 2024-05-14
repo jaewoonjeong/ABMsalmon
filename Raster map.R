2a51b7f80fe8f060c3139f998e3c5b3aedc36530
Jaewoon Jeong <45432619+jaewoonjeong@users.noreply.github.com>
2024-05-14 12:28:12 -0300

    Add files via upload

--- Raster map.R
+++ Raster map.R
@@ -1,63 +1,39 @@
-############# Raster 
-
-SL=read.csv(file='') # lice count data from salmon farms in British Columbia, Canada 
-SL=na.omit(SL, cols='Average.caligus.per.fish')
+library(gdistance); library(dplyr); library(lubridate); library(reshape); library(ggplot2); library(ggrepel)
+SL=read.csv(file='00_raw_data/industry data.csv'); SL=na.omit(SL, cols='Average.caligus.per.fish')
 SL=subset(SL,Finfish.Aquaculture.Management.Unit=='Discovery Islands')
 SL$Incident.Date=as.Date(SL$Incident.Date,"%d-%b-%y")
 SL$isoweek=isoweek(SL$Incident.Date)
-SL=subset(SL,Year==2018 & isoweek>17 & isoweek<27 & Longitude> -125.8)
+SL=subset(SL,isoweek>17 & isoweek<27 & Longitude> -125.8)
+SL=subset(SL, Year==2018)
+
 SL_unique=SL%>% distinct(Site.Common.Name, .keep_all = TRUE)
 
-LongMin=-126; LongMax=-124.7911; LatiMin=49.9; LatiMax=50.51
-PixelLong=1.411973/(10^3); PixelLati=0.8983153/(10^3)
-LongDivision=seq(LongMin,LongMax,PixelLong); LatiDivision=seq(LatiMin,LatiMax,PixelLati)
-
-PixelPoint=function(Point){c(LongDivision[which.min(abs(LongDivision-Point[1]))], LatiDivision[which.min(abs(LatiDivision-Point[2]))])}
-StartPoint=c(-125.020121, 49.939181); Destination=c(-125.813433, 50.392261); 
-SP=PixelPoint(StartPoint) # Start Point
-DP=PixelPoint(Destination) # Destination Point
-
-W=function(d){(1/(sqrt(2*3.14)*(Bandwidth/4)))*exp((-d^2)/(2*(Bandwidth/4)^2))} # Gaussian kernel density
-
-SHP=readOGR(dsn="") # coastline geographic file data in shp file
+#SHP=readOGR(dsn="00_raw_data/Water_Background.shp"); save(SHP,file='01_tidy_data/SHP.RData')
+load(file='01_tidy_data/SHP.RData')
 LongMin=-126; LongMax=-124.7911; LatiMin=49.9; LatiMax=50.51
 ext=extent(LongMin,LongMax,LatiMin,LatiMax)
 PixelLong=1.411973/(10^3); PixelLati=0.8983153/(10^3)
 rr <- raster(ext, res=c(PixelLong,PixelLati)) # length of border of a pixel = 100 meters
-r<-rasterize(SHP, rr)
+#r<-rasterize(SHP, rr); save(r, file='01_tidy_data/r.RData')
+load(file='01_tidy_data/r.RData')
+tr=transition(r,mean,directions=4)
+tr_Discovery=geoCorrection(tr, type="c")
 
+LongDivision=seq(LongMin,LongMax,PixelLong); LatiDivision=seq(LatiMin,LatiMax,PixelLati)
 LongID=1:length(LongDivision); LatiID=1:length(LatiDivision)
 FullDF=data.frame(Lon=rep(LongDivision,length(LatiDivision)),Lat=rep(LatiDivision,each=length(LongDivision)),LongID=rep(LongID,length(LatiDivision)), LatiID=rep(LatiID,each=length(LongDivision)))
-mm=matrix(NA, nrow = length(LongDivision),ncol = length(LatiDivision)); for(i in 1:length(LongDivision)){for(j in 1:length(LatiDivision)){mm[i,j]=r[length(LatiDivision)-j+1,i,1]}}; 
+#mm=matrix(NA, nrow = length(LongDivision),ncol = length(LatiDivision)); for(i in 1:length(LongDivision)){for(j in 1:length(LatiDivision)){mm[i,j]=r[length(LatiDivision)-j+1,i,1]}}; 
+#save(mm, file='01_tidy_data/mm.RData')
+load(file='01_tidy_data/mm.RData')
+
 DF=cbind(FullDF,Ocean=melt(mm)[,3]) # DF = all pixels
 df=subset(DF,Ocean==1)
-
-#Distance matrix
-cost_Discovery=matrix(NA,nrow(df),nrow(SL_unique)); for(j in 1:nrow(SL_unique)){for(i in 1:40000){cost_Discovery[i,j]=costDistance(tr_Discovery,rbind(as.numeric(df[i,c(1,2)]),as.numeric(SL_unique[j,c(7,6)])))}; print(j)}; 
-load(file='01_tidy_data/cost_Discovery1.RData'); cost_Discovery1=cost_Discovery; load(file='01_tidy_data/cost_Discovery2.RData'); cost_Discovery2=cost_Discovery; load(file='01_tidy_data/cost_Discovery3.RData'); cost_Discovery3=cost_Discovery; load(file='01_tidy_data/cost_Discovery4.RData'); cost_Discovery4=cost_Discovery
-cost_Discovery[1:40000,]=cost_Discovery1[1:40000,]; cost_Discovery[40001:80000,]=cost_Discovery2[40001:80000,]; cost_Discovery[80001:120000,]=cost_Discovery3[80001:120000,]; cost_Discovery[120001:nrow(cost_Discovery),]=cost_Discovery4[120001:nrow(cost_Discovery),]
+# Distance matrix; cost_Discovery=matrix(NA,nrow(df),nrow(SL_unique)); for(j in 1:nrow(SL_unique)){for(i in 1:40000){cost_Discovery[i,j]=costDistance(tr_Discovery,rbind(as.numeric(df[i,c(1,2)]),as.numeric(SL_unique[j,c(7,6)])))}; print(j)}; 
+#load(file='01_tidy_data/cost_Discovery1.RData'); cost_Discovery1=cost_Discovery; load(file='01_tidy_data/cost_Discovery2.RData'); cost_Discovery2=cost_Discovery; load(file='01_tidy_data/cost_Discovery3.RData'); cost_Discovery3=cost_Discovery; load(file='01_tidy_data/cost_Discovery4.RData'); cost_Discovery4=cost_Discovery
+#cost_Discovery[1:40000,]=cost_Discovery1[1:40000,]; cost_Discovery[40001:80000,]=cost_Discovery2[40001:80000,]; cost_Discovery[80001:120000,]=cost_Discovery3[80001:120000,]; cost_Discovery[120001:nrow(cost_Discovery),]=cost_Discovery4[120001:nrow(cost_Discovery),]
+#save(cost_Discovery, file='01_tidy_data/cost_Discovery.RData')
+load(file='01_tidy_data/cost_Discovery.RData')
 
 InfCost=which(cost_Discovery[,1]==Inf) # Inf in distance matrix
 DistanceMatrix=cost_Discovery[-InfCost,] # to remove rows with Inf
-
-d=DistanceMatrix/1000 # to change m to km
-Bandwidth=30
-dSum=matrix(NA,nrow(PixelInfo),nrow(SL_unique)); dim(dSum)=c(nrow(PixelInfo),nrow(SL_unique))
-ip=matrix(NA,nrow(PixelInfo),nrow(SL_unique)); dim(ip)=c(nrow(PixelInfo),nrow(SL_unique))
-for(i in 1:nrow(PixelInfo)){for(j in 1:nrow(SL_unique)){dSum[i,j]=W(d[i,j])/1000}}
-for(i in 1:nrow(PixelInfo)){for(j in 1:nrow(SL_unique)){ip[i,j]=dSum[i,j]/sum(dSum[,j])};print(i)}
-DistanceToEnd=numeric(); for(n in 1:nrow(PixelInfo)){DistanceToEnd[n]=costDistance(tr_Discovery,DP,as.numeric(PixelInfo[n,c(1,2)]))/1000; print(n)}
-#save(dSum,ip,Bandwidth, DistanceToEnd, R3DP1_DM, R3DP2_DM, file='01_tidy_data/ip.RData')
-load(file='01_tidy_data/ip.RData')
-
-LiceCountsPerFarm=62500000
-LiceCounts=LiceCountsPerFarm*8 # Lice counts from all farms
-iipp=matrix(NA,nrow(PixelInfo),nrow(SL_unique)*length(LiceCounts)); dim(iipp)=c(nrow(PixelInfo),nrow(SL_unique),length(LiceCounts))
-IP=matrix(NA,nrow(PixelInfo),length(LiceCounts))
-for(i in 1:nrow(PixelInfo)){for(j in 1:nrow(SL_unique)){for(s in 1:length(LiceCounts)){iipp[i,j,s]=ip[i,j]*LiceCounts[s]}};print(i)}
-for(i in 1:nrow(PixelInfo)){for(s in 1:length(LiceCounts)){IP[i,s]=sum(iipp[i,,s])};print(i)}
-
-Trawl=function(IP){exp(-5.49+log(0.0032141)+0.425*log(IP))} # To change IP to TP
-TP=Trawl(IP)
-
-PI=cbind(PixelInfo,IP=IP,TP=TP,DistanceToEnd=DistanceToEnd,R3DP1_DM=R3DP1_DM,R3DP2_DM=R3DP2_DM)
+PixelInfo=df[-InfCost,] # to remove rows with Inf
--- Sensitivity Analysis _ Proportional Model.R
+++ Sensitivity Analysis _ Proportional Model.R
@@ -39,7 +39,17 @@ SA_infes=function(r,c,t,g,b){
   NewInf=numeric()
   i=sample(1:iteration,1)
   for(n in 1:nrow(RR[[r]][[i]][[b]])){TP_SA=Trawl(PI_SA[[c]][[t]][which(RR[[r]][[i]][[b]][n,3]==PI_SA[[c]][[t]][,3] & RR[[r]][[i]][[b]][n,4]==PI_SA[[c]][[t]][,4]),]$IP)
-    NewInf[n]=rnbinom(1,size=0.921,mu=TP_SA)}
+  NewInf[n]=rnbinom(1,size=0.921,mu=TP_SA)}
+  return(sum(NewInf))
+}
+
+ProportionalModel=function(r,c,t,g,b){
+  Trawl=function(IP){exp(-11.6+log(0.0032141*gamma[g])+log(IP))}
+  if(r==1){b=4}
+  NewInf=numeric()
+  i=sample(1:iteration,1)
+  for(n in 1:nrow(RR[[r]][[i]][[b]])){TP_SA=Trawl(PI_SA[[c]][[t]][which(RR[[r]][[i]][[b]][n,3]==PI_SA[[c]][[t]][,3] & RR[[r]][[i]][[b]][n,4]==PI_SA[[c]][[t]][,4]),]$IP)
+  NewInf[n]=rnbinom(1,size=0.921,mu=TP_SA)}
   return(sum(NewInf))
 }
 ############################################################
@@ -51,62 +61,45 @@ g=sample(1:length(gamma),N, replace=TRUE)
 b=sample(1:nrow(beta),N, replace=TRUE)
 
 r=1;
-Output=numeric(); for(i in 1:N){Output[i]=SA_infes(r, c[i], t[i], g[i], b[i]);print(i)}
+Output=numeric(); for(i in 1:N){Output[i]=ProportionalModel(r, c[i], t[i], g[i], b[i]);print(i)}
 correlation_coefficient_c <- cor(c, Output, method="pearson"); correlation_coefficient_t <- cor(t, Output, method="pearson"); correlation_coefficient_g <- cor(g, Output, method="pearson"); correlation_coefficient_b <- cor(b, Output, method="pearson")
 input_names <- c("Number of sea lice","Bandwidth","Swimming speed", "Progression rate")
 coefficients <- c(correlation_coefficient_c, correlation_coefficient_t, correlation_coefficient_g, correlation_coefficient_b)
 df <- data.frame(Variable = input_names, Coefficient = coefficients)
 df1 <- df[order(abs(df$Coefficient), decreasing = TRUE), ]  # Order by absolute value for ranking
-#save(N, r, c, t, g, b, df1, Output, file='01_tidy_data/SA_result_1.RData')
+save(N, r, c, t, g, b, df1, Output, file='01_tidy_data/SA_result_1_ProportionalModel.RData')
 
 r=2
-Output=numeric(); for(i in 1:N){Output[i]=SA_infes(r, c[i], t[i], g[i], b[i]);print(i)}
+Output=numeric(); for(i in 1:N){Output[i]=ProportionalModel(r, c[i], t[i], g[i], b[i]);print(i)}
 correlation_coefficient_c <- cor(c, Output, method="pearson"); correlation_coefficient_t <- cor(t, Output, method="pearson"); correlation_coefficient_g <- cor(g, Output, method="pearson"); correlation_coefficient_b <- cor(b, Output, method="pearson")
 input_names <- c("Number of sea lice","Bandwidth","Swimming speed", "Progression rate")
 coefficients <- c(correlation_coefficient_c, correlation_coefficient_t, correlation_coefficient_g, correlation_coefficient_b)
 df <- data.frame(Variable = input_names, Coefficient = coefficients)
 df2 <- df[order(abs(df$Coefficient), decreasing = TRUE), ]  # Order by absolute value for ranking
-#save(N, r, c, t, g, b, df2, Output, file='01_tidy_data/SA_result_2.RData')
+save(N, r, c, t, g, b, df2, Output, file='01_tidy_data/SA_result_2_ProportionalModel.RData')
 
 r=3
-Output=numeric(); for(i in 1:N){Output[i]=SA_infes(r, c[i], t[i], g[i], b[i]);print(i)}
+Output=numeric(); for(i in 1:N){Output[i]=ProportionalModel(r, c[i], t[i], g[i], b[i]);print(i)}
 correlation_coefficient_c <- cor(c, Output, method="pearson"); correlation_coefficient_t <- cor(t, Output, method="pearson"); correlation_coefficient_g <- cor(g, Output, method="pearson"); correlation_coefficient_b <- cor(b, Output, method="pearson")
 input_names <- c("Number of sea lice","Bandwidth","Swimming speed", "Progression rate")
 coefficients <- c(correlation_coefficient_c, correlation_coefficient_t, correlation_coefficient_g, correlation_coefficient_b)
 df <- data.frame(Variable = input_names, Coefficient = coefficients)
 df3 <- df[order(abs(df$Coefficient), decreasing = TRUE), ]  # Order by absolute value for ranking
-#save(N, r, c, t, g, b, df3, Output, file='01_tidy_data/SA_result_3.RData')
+save(N, r, c, t, g, b, df3, Output, file='01_tidy_data/SA_result_3_ProportionalModel.RData')
 
 # pearson SA including Route
 r=sample(1:3,N, replace=TRUE)
 
-Output=numeric(); for(i in 1:N){Output[i]=SA_infes(r[i], c[i], t[i], g[i], b[i]);print(i)}
+Output=numeric(); for(i in 1:N){Output[i]=ProportionalModel(r[i], c[i], t[i], g[i], b[i]);print(i)}
 correlation_coefficient_c <- cor(c, Output, method="pearson"); correlation_coefficient_t <- cor(t, Output, method="pearson"); correlation_coefficient_r1_2 <- cor(r[which(r==1 | r==2)], Output[which(r==1 | r==2)], method="pearson")
 correlation_coefficient_g <- cor(g, Output, method="pearson"); correlation_coefficient_b <- cor(b, Output, method="pearson"); correlation_coefficient_r1_3 <- cor(r[which(r==1 | r==3)], Output[which(r==1 | r==3)], method="pearson")
 input_names <- c("Number of sea lice","Bandwidth","Swimming speed", "Progression rate", "Route 2", "Route 3")
 coefficients <- c(correlation_coefficient_c, correlation_coefficient_t, correlation_coefficient_g, correlation_coefficient_b, correlation_coefficient_r1_2, correlation_coefficient_r1_3)
 df <- data.frame(Variable = input_names, Coefficient = coefficients)
 df <- df[order(abs(df$Coefficient), decreasing = TRUE), ]  # Order by absolute value for ranking
-#save(N, r, c, t, g, b, df, Output, file='01_tidy_data/SA_result.RData')
+save(N, r, c, t, g, b, df, Output, file='01_tidy_data/SA_result_ProportionalModel.RData')
 
 #############################################################################################
-# results
-load(file='01_tidy_data/SA_result.RData')
-df$Variable <- reorder(df$Variable, abs(df$Coefficient))
-load(file='01_tidy_data/SA_result_1.RData') 
-df1=df1[-4,]; df1$Variable <- reorder(df1$Variable, abs(df1$Coefficient))
-load(file='01_tidy_data/SA_result_2.RData') 
-df2$Variable <- reorder(df2$Variable, abs(df2$Coefficient))
-load(file='01_tidy_data/SA_result_3.RData')
-df3$Variable <- reorder(df3$Variable, abs(df3$Coefficient))
-
-library(ggplot2); library(gridExtra)
-R0=ggplot(df,aes(x=Variable,y=Coefficient))+geom_col(fill=ifelse(df$Coefficient>0,"gray30","gray60"),width=0.6)+coord_flip()+labs(title="",x="",y="Pearson Correlation Coefficient")+theme_bw()+ggtitle('A-1')+scale_y_continuous(limits=c(-.5,.5))+theme(axis.title.x = element_text(size = 7), axis.text = element_text(size = 7),plot.margin = unit(c(0, .1, 0, .1), "in"))
-R1=ggplot(df1,aes(x=Variable,y=Coefficient))+geom_col(fill=ifelse(df1$Coefficient>0,"gray30","gray60"),width=0.6)+coord_flip()+labs(title="",x="",y="Pearson Correlation Coefficient")+theme_bw()+ggtitle('B-1')+scale_y_continuous(limits=c(-.5,.5))+theme(axis.title.x = element_text(size = 7), axis.text = element_text(size = 7),plot.margin = unit(c(0, .1, 0, .1), "in"))
-R2=ggplot(df2,aes(x=Variable,y=Coefficient))+geom_col(fill=ifelse(df2$Coefficient>0,"gray30","gray60"),width=0.6)+coord_flip()+labs(title="",x="",y="Pearson Correlation Coefficient")+theme_bw()+ggtitle('C-1')+scale_y_continuous(limits=c(-.5,.5))+theme(axis.title.x = element_text(size = 7), axis.text = element_text(size = 7),plot.margin = unit(c(0, .1, 0, .1), "in"))
-R3=ggplot(df3,aes(x=Variable,y=Coefficient))+geom_col(fill=ifelse(df3$Coefficient>0,"gray30","gray60"),width=0.6)+coord_flip()+labs(title="",x="",y="Pearson Correlation Coefficient")+theme_bw()+ggtitle('D-1')+scale_y_continuous(limits=c(-.5,.5))+theme(axis.title.x = element_text(size = 7), axis.text = element_text(size = 7),plot.margin = unit(c(0, .1, 0, .1), "in"))
-
-
 # results
 load(file='01_tidy_data/SA_result_ProportionalModel.RData')
 df$Variable <- reorder(df$Variable, abs(df$Coefficient))
@@ -118,15 +111,12 @@ load(file='01_tidy_data/SA_result_3_ProportionalModel.RData')
 df3$Variable <- reorder(df3$Variable, abs(df3$Coefficient))
 
 library(ggplot2); library(gridExtra)
-R0_ProportionalModel=ggplot(df,aes(x=Variable,y=Coefficient))+geom_col(fill=ifelse(df$Coefficient>0,"gray30","gray60"),width=0.6)+coord_flip()+labs(title="",x="",y="Pearson Correlation Coefficient")+theme_bw()+ggtitle('A-2')+scale_y_continuous(limits=c(-.75,.75))+theme(axis.title.x = element_text(size = 7), axis.text = element_text(size = 7),plot.margin = unit(c(0, .1, 0, .1), "in"))
-R1_ProportionalModel=ggplot(df1,aes(x=Variable,y=Coefficient))+geom_col(fill=ifelse(df1$Coefficient>0,"gray30","gray60"),width=0.6)+coord_flip()+labs(title="",x="",y="Pearson Correlation Coefficient")+theme_bw()+ggtitle('B-2')+scale_y_continuous(limits=c(-.75,.75))+theme(axis.title.x = element_text(size = 7), axis.text = element_text(size = 7),plot.margin = unit(c(0, .1, 0, .1), "in"))
-R2_ProportionalModel=ggplot(df2,aes(x=Variable,y=Coefficient))+geom_col(fill=ifelse(df2$Coefficient>0,"gray30","gray60"),width=0.6)+coord_flip()+labs(title="",x="",y="Pearson Correlation Coefficient")+theme_bw()+ggtitle('C-2')+scale_y_continuous(limits=c(-.75,.75))+theme(axis.title.x = element_text(size = 7), axis.text = element_text(size = 7),plot.margin = unit(c(0, .1, 0, .1), "in"))
-R3_ProportionalModel=ggplot(df3,aes(x=Variable,y=Coefficient))+geom_col(fill=ifelse(df3$Coefficient>0,"gray30","gray60"),width=0.6)+coord_flip()+labs(title="",x="",y="Pearson Correlation Coefficient")+theme_bw()+ggtitle('D-2')+scale_y_continuous(limits=c(-.75,.75))+theme(axis.title.x = element_text(size = 7), axis.text = element_text(size = 7),plot.margin = unit(c(0, .1, 0, .1), "in"))
+R0=ggplot(df,aes(x=Variable,y=Coefficient))+geom_col(fill=ifelse(df$Coefficient>0,"gray30","gray60"),width=0.6)+coord_flip()+labs(title="",x="",y="Pearson Correlation Coefficient")+theme_bw()+ggtitle('')+scale_y_continuous(limits=c(-.75,.75))+theme(axis.title.x = element_text(size = 7), axis.text = element_text(size = 7),plot.margin = unit(c(0, .1, 0, .1), "in"))
+R1=ggplot(df1,aes(x=Variable,y=Coefficient))+geom_col(fill=ifelse(df1$Coefficient>0,"gray30","gray60"),width=0.6)+coord_flip()+labs(title="",x="",y="Pearson Correlation Coefficient")+theme_bw()+ggtitle('')+scale_y_continuous(limits=c(-.75,.75))+theme(axis.title.x = element_text(size = 7), axis.text = element_text(size = 7),plot.margin = unit(c(0, .1, 0, .1), "in"))
+R2=ggplot(df2,aes(x=Variable,y=Coefficient))+geom_col(fill=ifelse(df2$Coefficient>0,"gray30","gray60"),width=0.6)+coord_flip()+labs(title="",x="",y="Pearson Correlation Coefficient")+theme_bw()+ggtitle('')+scale_y_continuous(limits=c(-.75,.75))+theme(axis.title.x = element_text(size = 7), axis.text = element_text(size = 7),plot.margin = unit(c(0, .1, 0, .1), "in"))
+R3=ggplot(df3,aes(x=Variable,y=Coefficient))+geom_col(fill=ifelse(df3$Coefficient>0,"gray30","gray60"),width=0.6)+coord_flip()+labs(title="",x="",y="Pearson Correlation Coefficient")+theme_bw()+ggtitle('')+scale_y_continuous(limits=c(-.75,.75))+theme(axis.title.x = element_text(size = 7), axis.text = element_text(size = 7),plot.margin = unit(c(0, .1, 0, .1), "in"))
 
 library(patchwork)
-A=R0+(R1/R2/R3)+plot_annotation(tag_levels='A')
-B=R0_ProportionalModel+(R1_ProportionalModel/R2_ProportionalModel/R3_ProportionalModel)+plot_annotation(tag_levels='A')
-
-tiff(filename = "03_plots/Fig. 7.tif", width = 19.05, height = 20, units = 'cm', res = 600)
-A/B
+tiff(filename = "03_plots/Fig 8.tiff", width = 19.05, height = 10, units = 'cm', res = 600)
+R0+(R1/R2/R3)+plot_annotation(tag_levels='A')
 dev.off()
